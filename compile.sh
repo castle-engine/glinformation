5e74766c7d70cadb9eb3cbe0681e863920db5ba4
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-03-05 11:10:04 +0100

    Build with castle-engine tool.

--- compile.sh
+++ compile.sh
@@ -1,18 +1,27 @@
 #! /bin/sh
 set -eu
 
-# This is automatically generated script that should compile
-# all programs in this archive. It simply calls FPC
-# with proper command-line options.
-#
-# We must do cd ../castle_game_engine/ (and call FPC from that directory)
-# because castle-fpc.cfg file is there and it contains paths relative
-# to that directory.
+# Compile glinformation and glinformation_glut binaries.
 
-cd ../castle_game_engine/
+if [ -d ../castle_game_engine ]; then
+  # Force rebuilding CastleWindow unit with proper backend.
+  cd ../castle_game_engine/
+  make --quiet clean-window
+  cd ../glinformation/
+fi
 
-# Force rebuilding CastleWindow unit with proper backend.
-make --quiet clean-window
-
-fpc -dRELEASE ${CASTLE_FPC_OPTIONS:-} @castle-fpc.cfg ../glinformation/glinformation.lpr
-fpc -dRELEASE ${CASTLE_FPC_OPTIONS:-} @castle-fpc.cfg ../glinformation/glinformation_glut.lpr
+if which castle-engine  > /dev/null; then
+  # preferred build option: use castle-engine build tool.
+  castle-engine simple-compile ${CASTLE_ENGINE_TOOL_OPTIONS:-} glinformation.lpr
+  castle-engine simple-compile ${CASTLE_ENGINE_TOOL_OPTIONS:-} glinformation_glut.lpr
+else
+  # fallback build option: directly call FPC,
+  # assuming CGE is in ../castle_game_engine/ .
+  #
+  # We must do cd ../castle_game_engine/ (and call FPC from that directory)
+  # because castle-fpc.cfg file is there and it contains paths relative
+  # to that directory.
+  cd ../castle_game_engine/
+  fpc -dRELEASE ${CASTLE_FPC_OPTIONS:-} @castle-fpc.cfg ../glinformation/glinformation.lpr
+  fpc -dRELEASE ${CASTLE_FPC_OPTIONS:-} @castle-fpc.cfg ../glinformation/glinformation_glut.lpr
+fi
